1aaf61288571a2e3170a021fb0012b947803dd0c
kirill_masanov <kirill_masanov@mail.ru>
2020-04-16 20:14:48 +0300

    Add SQLAlchemy, create model

---
+++ webapp/config.py
@@ -0,0 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WEATHER_DEFAULT_CITY = 'Moscow,Russia'
+WEATHER_API_KEY = '9c1d10e844d04c78831174833201204'
+WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
