c3aaf08a3dd891410ebface894601926d74f9e39
kirill_masanov <kirill_masanov@mail.ru>
2020-04-27 18:24:01 +0300

    Create login form

--- webapp/config.py
+++ webapp/config.py
@@ -6,3 +6,5 @@ WEATHER_DEFAULT_CITY = 'Moscow,Russia'
 WEATHER_API_KEY = '9c1d10e844d04c78831174833201204'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SECRET_KEY = 'wdqdwd2e@e2jeiji8904tl@!kdkkbn8@&$^09k'
