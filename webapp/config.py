e6301583d159b492a7ada9d92c4a7e75b73b679d
kirill_masanov <kirill_masanov@mail.ru>
2020-05-05 15:47:23 +0300

    add the functionality of the remember user

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,3 +9,5 @@ WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 
 SECRET_KEY = 'wdqdwd2e@e2jeiji8904tl@!kdkkbn8@&$^09k'
+
+REMEMBER_COOKIE_DURATION = timedelta(days=5)
