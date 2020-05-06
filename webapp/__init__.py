e1e7b231f726cb19ba36dcd847f818d98208dabb
kirill_masanov <kirill_masanov@mail.ru>
2020-05-06 22:08:00 +0300

    add blueprint user

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, flash, render_template, redirect, url_for
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask import Flask, render_template
+from flask_login import LoginManager, current_user, login_required
 
-from webapp.forms import LoginForm
-from webapp.model import db, News, User
+
+from webapp.model import db, News
+from webapp.user.models import User
+from webapp.user.views import blueprint as user_blueprint
 from webapp.weather import weather_by_city
 
 
@@ -14,7 +16,8 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+    app.register_blueprint(user_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -26,34 +29,6 @@ def create_app():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
-
-    @app.route('/login')
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        title = 'Авторизация'
-        login_form = LoginForm()
-        return render_template('login.html', page_title=title, form=login_form)
-
-    @app.route('/process-login', methods=['POST'])
-    def process_login():
-        form = LoginForm()
-
-        if form.validate_on_submit():
-            user = User.query.filter(User.username == form.username.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user, remember=form.remember_me.data)
-                flash('Вы успешно вошли на сайт')
-                return redirect(url_for('index'))
-
-        flash('Неправильные имя или пароль')
-        return redirect(url_for('login'))
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        flash('Вы успешно разлогинились!')
-        return redirect(url_for('index'))
 
     @app.route('/admin')
     @login_required
