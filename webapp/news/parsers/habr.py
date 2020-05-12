d65c6c30fab1bc2dd15be28b6816ccace7ddbd3b
kirill_masanov <kirill_masanov@mail.ru>
2020-05-13 00:03:02 +0300

    change parser python_news to habr_news

---
+++ webapp/news/parsers/habr.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+import locale
+import platform
+
+from bs4 import BeautifulSoup
+
+from webapp.news.parsers.utils import get_html, save_news
+
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, 'russian')
+else:
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+
+def parse_habr_date(date_str):
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    elif 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
+
+
+def get_habr_snippets():
+    url = 'https://habr.com/ru/search/?target_type=posts&q=python&order_by=date'
+    html = get_html(url)
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_news = soup.find('ul', class_='content-list_posts').find_all('li', class_='content-list__item_post')
+        result_news = []
+        for news in all_news:
+            title = news.find('a', class_='post__title_link').text
+            url = news.find('a', class_='post__title_link')['href']
+            published = news.find('span', class_='post__time').text
+            published = parse_habr_date(published)
+            save_news(title, url, published)
