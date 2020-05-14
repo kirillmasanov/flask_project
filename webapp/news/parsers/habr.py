4538a2a936086ba85ce2957364e4412d8d78c12b
kirill_masanov <kirill_masanov@mail.ru>
2020-05-14 19:35:14 +0300

    add the functionality of news aggregation from habr.com

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -4,12 +4,29 @@ import platform
 
 from bs4 import BeautifulSoup
 
+from webapp.db import db
+from webapp.news.models import News
 from webapp.news.parsers.utils import get_html, save_news
 
 if platform.system() == 'Windows':
     locale.setlocale(locale.LC_ALL, 'russian')
 else:
-    locale.setlocale(locale.LC_TIME, 'ru_RU')
+    locale.setlocale(locale.LC_TIME, 'ru_RU.utf8')
+
+dict_month = {
+    'января': 'Январь',
+    'февраля': 'Февраль',
+    'марта': 'Март',
+    'апреля': 'Апрель',
+    'мая': 'Май',
+    'июня': 'Июнь',
+    'июля': 'Июль',
+    'августа': 'Август',
+    'сентября': 'Сентябрь',
+    'октября': 'Октябрь',
+    'ноября': 'Ноябрь',
+    'декабря': 'Декабрь'
+}
 
 
 def parse_habr_date(date_str):
@@ -19,13 +36,19 @@ def parse_habr_date(date_str):
     elif 'вчера' in date_str:
         yesterday = datetime.now() - timedelta(days=1)
         date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    else:
+        date_list = date_str.split()
+        month = date_list[1]
+        if month in dict_month:
+            date_list[1] = dict_month[month]
+            date_str = ' '.join(date_list)
     try:
         return datetime.strptime(date_str, '%d %B %Y в %H:%M')
     except ValueError:
         return datetime.now()
 
 
-def get_habr_snippets():
+def get_news_snippets():
     url = 'https://habr.com/ru/search/?target_type=posts&q=python&order_by=date'
     html = get_html(url)
     if html:
@@ -38,3 +61,16 @@ def get_habr_snippets():
             published = news.find('span', class_='post__time').text
             published = parse_habr_date(published)
             save_news(title, url, published)
+
+
+def get_news_content():
+    news_without_text = News.query.filter(News.text.is_(None))
+    for news in news_without_text:
+        html = get_html(news.url)
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            article = soup.find('div', class_='post__text-html').decode_contents()
+            if article:
+                news.text = article
+                db.session.add(news)
+                db.session.commit()
