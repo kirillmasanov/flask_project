df3dbf1236c5cad0d6d6104c08a244b7be607bf9
kirill_masanov <kirill_masanov@mail.ru>
2020-05-16 14:07:59 +0300

    add model Comments

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from webapp.db import db
 
 
@@ -10,3 +12,22 @@ class News(db.Model):
 
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
