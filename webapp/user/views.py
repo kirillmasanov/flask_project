7e9a8e53f137490f9f58dfb64cc80dcc30e4cbaa
kirill_masanov <kirill_masanov@mail.ru>
2020-05-17 16:04:28 +0300

    add redirect after login

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user
 from webapp import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -11,7 +12,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
@@ -26,7 +27,8 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())  # error ERR_TOO_MANY_REDIRECTS if login from main page
+            # return redirect(url_for('news.index'))
 
     flash('Неправильные имя или пароль')
     return redirect(url_for('user.login'))
